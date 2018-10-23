861acf668b9681b4441f71a6a2b7b7f808ab013d
Juice <ryanp.burns10@gmail.com>
2018-10-23 16:14:03 -0400

    initial files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# triviaGame
+Here I used a simple interval timer for the Game. I used classes to designate correct and incorrect answers. On click of .correct the score 
+increments by 1. 
+
+I will come back to this because I'd like to tweak the CSS for better alignment, I also plan on adding CSS through JS that clearly indicates 
+that a button has been clicked. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,27 @@
+var gameTime = setTimeout(gametime, 1000 * 30);
+
+var converted = stopwatch.timeConverter(stopwatch.time);
+    console.log(converted);
+
+    // DONE: Use the variable we just created to show the converted time in the "display" div.
+    $("#display").text(converted);
+
+function(t) { 
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
 
